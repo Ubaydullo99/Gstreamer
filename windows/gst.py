ccfbeb6634a4a3d7873d2dde4fcf0049744a303f
Ubaydullo Asatullaev <Ubaydullohazard@gmail.com>
2023-08-03 15:40:43 +0900

    Create gst.py

---
+++ windows/gst.py
@@ -0,0 +1,27 @@
+# Installing gstreamer 
+
+# https://gstreamer.freedesktop.org/download/ 
+# ref: https://gstreamer.freedesktop.org/documentation/installing/on-windows.html?gi-language=c
+
+# after install and put plugin to path 
+# set  "C:\gstreamer\1.0\mingw_x86_64\bin" plugin to windows path by 
+
+# searching Edit environment variables,  and follow:
+# / System Properties/ Environment Variables/used variables for "Username"/ Path -  create new path - "C:\gstreamer\1.0\mingw_x86_64\bin". 
+
+gst-inspect-1.0
+# 200 plugins, 1532 features are found
+
+# Installing actaul gstreamer plugin to see gstreamer inside obs studio
+# OBS studion gstreamer      https://obsproject.com/download
+# tutorial:   https://www.youtube.com/watch?v=qZWFuTCKNeQ
+# install obs-gstreamer.zip:     https://github.com/fzwoch/obs-gstreamer/releases/tag/v0.3.0
+
+# copy obs-gstreamer.dll file and paste inside "C:\Program Files\obs-studio\obs-plugins\64bit", and now gstreamer source will show up in obs studion app.
+
+
+
+
+
+# for Gstreamer projects 
+# https://www.youtube.com/playlist?list=PLfFan4sDonOccKApSxnb8mZyUS3CLxSMF
